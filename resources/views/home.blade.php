02391cf942d5fee0f7ffaba4a2a1e483f4666f52
Edson Chivambo <echivambo@PSIMOZ.local>
2017-11-30 14:34:52 +0200

    Layout pronto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layout')
+@section('content')
+
+    <!-- Page Content -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
+
+
+
+        </div>
+        <footer class="footer text-center">
+            2017 © Copyright Logistica | PSI Moz. <a href="http://evidevi.com/" target="_blank">Powered by.: evidevi.com</a>
+        </footer>
+    </div>
+    <!-- /#page-wrapper -->
+
+@endsection()
